79c00ed4907298fa61ddfc22bea13719d7db72f3
dganicolas <dganicolas@iesrafaelalberti.es>
2024-05-07 20:01:29 +0200

    error en la linea 277 y 77

---
+++ src/main/kotlin/IViewModel.kt
@@ -0,0 +1,24 @@
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+
+interface IViewModel {
+    val ficheros: IGestorFicheros
+    val _lista: MutableList<String>
+    val lista: MutableList<String>
+    var _toasta: MutableState<Boolean>
+    val toasta: MutableState<Boolean>
+    var _mensaje: MutableState<String>
+    val mensaje: MutableState<String>
+    var _textState: MutableState<String>
+    val textState: State<String>
+    var _ventanatexto: MutableState<Boolean>
+    val ventanatexto: MutableState<Boolean>
+    fun refrescarTexto(texto: String)
+    fun refrescarMensaje(texto: String)
+    fun refrescarToasta(estado: Boolean)
+    fun eliminarEstudiante(posicion: Int)
+    fun agregarEstudiante(estudiante: String)
+    fun guardarEstudiantes(): Boolean
+    fun refrescartextoestudiante(texto: String)
+    fun refrescarEstadoPantallaEstudiante(estado: Boolean)
+}
--- src/main/kotlin/ViewModel.kt
+++ src/main/kotlin/ViewModel.kt
@@ -2,52 +2,57 @@ import androidx.compose.runtime.*
 import java.io.File
 
 class ViewModel(
-    val ficheros: IGestorFicheros
-) {
-    private val _lista = ficheros.leer(File("src/main/recursos/alumnos.txt"))
-    val lista = _lista
+    override val ficheros: IGestorFicheros
+) : IViewModel {
+    override val _lista = ficheros.leer(File("src/main/recursos/alumnos.txt"))
+    override val lista = _lista
 
-    private var _toasta = mutableStateOf(false)
-    val toasta = _toasta
+    override var _toasta = mutableStateOf(false)
+    override val toasta = _toasta
 
-    private var _mensaje = mutableStateOf("")
-    val mensaje = _mensaje
+    val _posicion = mutableStateOf(0)
+    val posicion = _posicion
+    override var _mensaje = mutableStateOf("")
+    override val mensaje = _mensaje
 
-    private var _textState = mutableStateOf("")
-    val textState: State<String> = _textState
+    override var _textState = mutableStateOf("")
+    override val textState: State<String> = _textState
 
-    private var _ventanatexto = mutableStateOf(false)
-    val ventanatexto = _ventanatexto
+    override var _ventanatexto = mutableStateOf(false)
+    override val ventanatexto = _ventanatexto
 
-    fun refrescarTexto(texto: String) {
+    override fun refrescarTexto(texto: String) {
         _textState.value = texto
     }
 
-    fun refrescarMensaje(texto: String) {
+    override fun refrescarMensaje(texto: String) {
         _mensaje.value = texto
     }
 
-    fun refrescarToasta(estado: Boolean) {
+    override fun refrescarToasta(estado: Boolean) {
         _toasta.value = estado
     }
 
-    fun eliminarEstudiante(posicion: Int) {
+    override fun eliminarEstudiante(posicion: Int) {
         _lista.removeAt(posicion)
     }
 
-    fun agregarEstudiante(estudiante: String) {
+    override fun agregarEstudiante(estudiante: String) {
         _lista.add(estudiante)
     }
 
-    fun guardarEstudiantes():Boolean{
+    override fun guardarEstudiantes():Boolean{
             return ficheros.escribir(File("src/main/recursos/alumnos.txt"),_lista)
     }
 
-    fun refrescartextoestudiante(posicion:Int,texto:String){
-        _lista[posicion]=texto
+    override fun refrescartextoestudiante(texto:String){
+        _lista[posicion.value]=texto
     }
 
-    fun refrescarEstadoPantallaEstudiante(estado:Boolean){
+    fun refrescarPosicion(posicion: Int){
+        _posicion.value = posicion
+    }
+    override fun refrescarEstadoPantallaEstudiante(estado:Boolean){
         _ventanatexto.value = estado
     }
 }
