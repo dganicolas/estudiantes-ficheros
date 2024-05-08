8cc9ada38f393f98dc43f6bca93b8fc2b81398b9
dganicolas <dganicolas@rafaelalberti.es>
2024-05-08 04:05:46 +0200

    no ewntiendo la parte de la base de datos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,50 @@
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 
 fun main() = application {
     val gestor = GestorFicheros()
-    val viewModel = ViewModel(gestor)
+    val ventanaEleccion = mutableStateOf(true)
+    val opcionsql = mutableStateOf(false)
+    val opcionlocal = mutableStateOf(false)
     Window(onCloseRequest = ::exitApplication) {
-        ventanaPrincipal(viewModel)
-    }
+        if (ventanaEleccion.value) {
+            ventanaEleccion(
+                onclickAceptarOpcion = { ventanaEleccion.value = false },
+                opcionsql = opcionsql.value,
+                opciontxt = opcionlocal.value,
+                oncheckedOpcionSql = {
+                    if (!opcionsql.value) {
+                        opcionsql.value = true
+                        opcionlocal.value = false
+                    }else{
+                        opcionsql.value = false
+                        opcionlocal.value = false
+                    }
 
+                },
+                oncheckedOpcionTxt = {
+                        if (!opcionlocal.value) {
+                            opcionlocal.value = true
+                            opcionsql.value = false
+                        }else{
+                            opcionlocal.value = false
+                            opcionsql.value = false
+                        }
+
+                }
+            )
+        } else {
+            val viewModel = if(opcionsql.value ){
+                studentViewModelDb(gestor)
+            }else{
+                ViewModel(gestor)
+
+            }
+            ventanaPrincipal(viewModel)
+        }
+     }
 }
+
+
